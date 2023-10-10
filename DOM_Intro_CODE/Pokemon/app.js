ddb6290e6e06d1deb364e5255ec0f7d66e1964b7
Aakif <aakif.msiddiqui@gmail.com>
2023-10-11 00:22:00 +0530

    add pokemon sprite

--- DOM_Intro_CODE/Pokemon/app.js
+++ DOM_Intro_CODE/Pokemon/app.js
@@ -1,19 +1,13 @@
-// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
-
 const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-
-
-for (let i = 1; i <= 151; i++) {
+const baseUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+for(let i = 1; i < 151; i++) {
     const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
     const label = document.createElement('span');
     label.innerText = `#${i}`;
+    pokemon.classList.add('pokemon');
     const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
-
-
+    newImg.src = `${baseUrl}${i}.png`;
     pokemon.appendChild(newImg);
     pokemon.appendChild(label);
-    container.appendChild(pokemon)
+    container.appendChild(pokemon);
 }
